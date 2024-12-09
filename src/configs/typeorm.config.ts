66dc3b7c94fead46033e66cb7f417327ee37481a
sajad ahmadnzhad <ahmadnzhad22@gmail.com>
2024-12-09 16:35:26 +0330

    feat: config database with typeorm

---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,22 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export default (): TypeOrmModuleOptions => {
+    const {
+        DB_HOST,
+        DB_NAME,
+        DB_PORT,
+        DB_SYNCHRONIZE,
+        DB_USERNAME,
+        DB_PASSWORD,
+    } = process.env
+    return {
+        type: "postgres",
+        database: DB_NAME,
+        host: DB_HOST,
+        port: Number.parseInt(DB_PORT),
+        username: DB_USERNAME,
+        password: DB_PASSWORD,
+        synchronize: !!Number.parseInt(DB_SYNCHRONIZE),
+        autoLoadEntities: true,
+    }
+}
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RolesModule } from '../roles/roles.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeormConfig from '../../configs/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RolesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.cwd() + '/.env'
+    }),
+    TypeOrmModule.forRoot(typeormConfig()),
+    RolesModule
+  ],
   controllers: [],
   providers: [],
 })
